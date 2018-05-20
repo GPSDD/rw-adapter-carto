78c5c1f3b423a6ac49d7abeecbe6147d59030cc6
Tiago Garcia <tiagojsag@gmail.com>
2018-05-20 11:50:31 +0200

    Fix tests

--- app/test/e2e/carto.spec.js
+++ app/test/e2e/carto.spec.js
@@ -1,11 +1,14 @@
 const logger = require('logger');
 const assert = require('assert');
-const request = require('superagent').agent();
 const nock = require('nock');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+let requester;
+
+chai.use(chaiHttp);
+
 
 require('should');
-let instance = null;
-let BASE_URL = null;
 
 const dataset = {
     data: {
@@ -27,63 +30,66 @@ const fields = [{
 }];
 
 describe('E2E test', () => {
-    // before(() => {
-    //     const app = require('app');
-    //     BASE_URL = `http://localhost:${app.address().port}`;
+    before(() => {
 
-    //     nock(`https://wri-01.carto.com`)
-    //         .get(encodeURI(`/api/v2/sql?q=select * from ${dataset.data.attributes.table_name} limit 0`))
-    //         .reply(200, {
-    //             rows: [],
-    //             fields
-    //         });
-    //     nock(`${process.env.CT_URL}`)
-    //         .get(encodeURI(`/convert/sql2SQL?sql=update table`))
-    //         .reply(400, {
-    //             status: 400,
-    //             detail: 'Malformed query'
-    //         });
-    // });
+        nock(`${process.env.CT_URL}`)
+            .persist()
+            .post(`/api/v1/microservice`)
+            .reply(200);
 
-    // it('Get fields correctly', async() => {
-    //     let response = null;
-    //     try {
-    //         response = await request.post(`${BASE_URL}/api/v1/carto/fields/${dataset.data.id}`).send({
-    //             dataset,
-    //             loggedUser: null
-    //         });
-    //     } catch (e) {
-    //         logger.error(e);
-    //         assert(false, 'Exception throwed');
-    //     }
-    //     response.status.should.equal(200);
-    //     response.body.should.have.property('tableName');
-    //     response.body.tableName.should.equal(dataset.data.attributes.table_name);
-    //     response.body.should.have.property('fields');
-    //     response.body.fields.should.deepEqual(fields);
-    // });
+        const server = require('../../src/app');
+        requester = chai.request(server).keepOpen();
 
-    // it('Do query with query invalid', async() => {
-    //     let response = null;
-    //     try {
-    //         response = await request.post(`${BASE_URL}/api/v1/carto/query/${dataset.data.id}?sql=update table`).send({
-    //             dataset,
-    //             loggedUser: null
-    //         });
-    //         assert(false, 'Exception NOT throwed');
-    //     } catch (e) {
-    //         e.status.should.equal(400);
-    //         e.response.body.should.have.property('status');
-    //         e.response.body.status.should.equal(400);
-    //         e.response.body.should.have.property('detail');
-    //         e.response.body.detail.should.equal('Malformed query');
-    //     }
+        nock(`https://wri-01.carto.com`)
+            .get(encodeURI(`/api/v2/sql?q=select * from ${dataset.data.attributes.table_name} limit 0`))
+            .reply(200, {
+                rows: [],
+                fields
+            });
+        nock(`${process.env.CT_URL}`)
+            .get(encodeURI(`/v1/convert/sql2SQL`))
+            .query({ sql: 'update table', experimental: 'true', raster: 'false' })
+            .reply(400, {
+                status: 400,
+                detail: 'Malformed query'
+            });
+    });
 
-    // });
+    it('Get fields correctly', async () => {
+        let response = null;
+        try {
+            response = await requester
+                .post(`/api/v1/carto/fields/${dataset.data.id}`)
+                .send({
+                    dataset,
+                    loggedUser: null
+                });
+        } catch (e) {
+            logger.error(e);
+            assert(false, 'Exception thrown');
+        }
+        response.status.should.equal(200);
+        response.body.should.have.property('tableName');
+        response.body.tableName.should.equal(dataset.data.attributes.table_name);
+        response.body.should.have.property('fields');
+        response.body.fields.should.deepEqual(fields);
+    });
 
-    // after(() => {
-    //     if (instance) {
-    //         instance.close();
-    //     }
-    // });
+    it('Do query with query invalid', async () => {
+        const response = await requester
+            .post(`/api/v1/carto/query/${dataset.data.id}?sql=update table`)
+            .send({
+                dataset,
+                loggedUser: null
+            });
+        response.status.should.equal(400);
+        response.body.should.have.property('status');
+        response.body.status.should.equal(400);
+        response.body.should.have.property('detail');
+        response.body.detail.should.equal('Malformed query');
+
+    });
+
+    after(() => {
+    });
 });
